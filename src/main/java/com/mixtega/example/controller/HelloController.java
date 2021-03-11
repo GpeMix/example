dc1d6d3d09bb0e5b35b5c128ad2dbe41c072e7ad
GpeMixtega <lupitamixtega@gmail.com>
2021-03-10 21:33:35 -0600

    Segundo cambio, prueba

--- src/main/java/com/mixtega/example/controller/HelloController.java
+++ src/main/java/com/mixtega/example/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String getHelloWorld(){
-       return "Hello World w/Spring";
+       return "Hello World with/Spring";
     }
 }
