2c8cc282028a1d816e6e9021527aba3e149b719c
GpeMixtega <lupitamixtega@gmail.com>
2021-03-10 21:30:23 -0600

    Commit inicial, Estructura  base

---
+++ src/main/java/com/mixtega/example/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.mixtega.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String getHelloWorld(){
+       return "Hello World w/Spring";
+    }
+}
